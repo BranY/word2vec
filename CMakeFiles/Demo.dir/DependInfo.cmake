e0e28a1363aa29bf0c71f376e09acc596aa1d159
BranY <yangwen_jia@163.com>
2020-04-25 21:41:02 +0800

    add cmakelist

---
+++ CMakeFiles/Demo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/yangwenjia/workspace/source/word2vec/src/compute-accuracy.c" "/Users/yangwenjia/workspace/source/word2vec/CMakeFiles/Demo.dir/src/compute-accuracy.o"
+  "/Users/yangwenjia/workspace/source/word2vec/src/distance.c" "/Users/yangwenjia/workspace/source/word2vec/CMakeFiles/Demo.dir/src/distance.o"
+  "/Users/yangwenjia/workspace/source/word2vec/src/word-analogy.c" "/Users/yangwenjia/workspace/source/word2vec/CMakeFiles/Demo.dir/src/word-analogy.o"
+  "/Users/yangwenjia/workspace/source/word2vec/src/word2phrase.c" "/Users/yangwenjia/workspace/source/word2vec/CMakeFiles/Demo.dir/src/word2phrase.o"
+  "/Users/yangwenjia/workspace/source/word2vec/src/word2vec.c" "/Users/yangwenjia/workspace/source/word2vec/CMakeFiles/Demo.dir/src/word2vec.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/src/compute-accuracy.o"
+  "CMakeFiles/Demo.dir/src/distance.o"
+  "CMakeFiles/Demo.dir/src/word-analogy.o"
+  "CMakeFiles/Demo.dir/src/word2phrase.o"
+  "CMakeFiles/Demo.dir/src/word2vec.o"
+  "Demo.pdb"
+  "Demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
